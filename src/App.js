57cfb021671b868bf71f6e523434f2ddda177763
mchmielecki_252723 <mchmielecki_252723@e-science.pl>
2022-09-13 17:46:00 +0200

    working login page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from './Login';
+
+function App() {
+
+  return (
+    <div>
+      <h2>Solar Monitoring</h2>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,19 @@
+function Login() {
+    return (
+        <div>
+            <div>
+                <label>Login:</label>
+                <input type='text'/>
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type='password'/>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
