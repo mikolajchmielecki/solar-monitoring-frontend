5774d94007c06027e8d41ee8c49a83b13708ceb7
mchmielecki_252723 <mchmielecki_252723@e-science.pl>
2022-11-19 13:00:53 +0100

    change ip

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
-//export const SERVER_URL = 'http://34.116.171.10:9285/api/v1/'
-export const SERVER_URL = 'https://localhost:8000/api/v1/'
+export const SERVER_URL = 'https://34.116.171.10:9285/api/v1/'
+//export const SERVER_URL = 'https://localhost:8000/api/v1/'
 
 export const InverterType = {
     Solax: 'solax',
     SolarEdge: 'solar-edge'
-  }
+}
