ea8fd9f89f9d1146a2620e59c1f93171fa107bf1
mchmielecki_252723 <mchmielecki_252723@e-science.pl>
2022-11-23 22:42:55 +0100

    ortographic issue

--- src/components/InverterCard.js
+++ src/components/InverterCard.js
@@ -34,7 +34,7 @@ export default function InverterCard ({id, inverter, handleDelete}) {
         <ListGroup variant="flush">
           <ListGroup.Item>Status: <strong>{inverter.inverterParameters.status}</strong></ListGroup.Item>
           <ListGroup.Item>Marka: <strong>{inverter.type===Constants.InverterType.SolarEdge ? "SolarEdge" : "Solax"}</strong></ListGroup.Item>
-          <ListGroup.Item>Bierząca moc: {valid ? <strong>{round(inverter.inverterParameters.currentPower)} W</strong> : <strong>---</strong>}</ListGroup.Item>
+          <ListGroup.Item>Bieżąca moc: {valid ? <strong>{round(inverter.inverterParameters.currentPower)} W</strong> : <strong>---</strong>}</ListGroup.Item>
           <ListGroup.Item>Dzisiejsza produkcja: {valid ? <strong>{round(inverter.inverterParameters.todayYield)} kWh</strong> : <strong>---</strong>}</ListGroup.Item>
           <ListGroup.Item>Całkowita produkcja: {valid ? <strong>{round(inverter.inverterParameters.totalYield)} kWh</strong> : <strong>---</strong>}</ListGroup.Item>
         </ListGroup>
